69feff007f4962a33be44f9f01e9fabcb5172bd2
Anurag Raina <rainakalhan90@gmail.com>
2020-08-08 13:01:40 +0530

    Debug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,7 +18,7 @@ const useStyles = makeStyles(() => ({
 		backgroundColor : '#fff',
 		marginTop       : '7rem',
 		marginBottom    : '5rem',
-		minHeight       : '5,00px',
+		minHeight       : '500px',
 		borderRadius    : '6px',
 		boxShadow       :
 			'0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)'
